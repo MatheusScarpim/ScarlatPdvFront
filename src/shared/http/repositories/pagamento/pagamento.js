6287387c2dbba1a2322214d2843f3f3a63bd2098
Christoffer Mesquita <paysunset0++@gmail.com>
2025-03-28 14:13:44 -0300

    fix: API para pagamento resolvido

--- src/shared/http/repositories/pagamento/pagamento.js
+++ src/shared/http/repositories/pagamento/pagamento.js
@@ -5,11 +5,13 @@ const APICore = "http://localhost:4000";
 const CreatePixPayment = (data = {}, config = {}) => axios.post(`${APICore}/payments/pix`, data, config);
 const GetPaymentStatus = (paymentId, config = {}) => axios.get(`${APICore}/payments/status/${paymentId}`, config);
 const GetDashboardData = (config = {}) => axios.get(`${APICore}/payments/vendas/dashboard`, config);
+const ProcessCardPayment = (data = {}, config = {}) => axios.post(`${APICore}/payments/point`, data, config);
 
 const PagamentoRepository = {
     CreatePixPayment,
     GetPaymentStatus,
-    GetDashboardData
+    GetDashboardData,
+    ProcessCardPayment
 };
 
 export default PagamentoRepository; 
